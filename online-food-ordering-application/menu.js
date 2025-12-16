a208eb316299d5acd8f0c4ab171b64bdd8b822d0
Tiberiu Mitiloaga <tmitiloaga80@gmail.com>
2025-12-16 11:10:31 +0200

    add styles for cart

--- online-food-ordering-application/menu.js
+++ online-food-ordering-application/menu.js
@@ -21,32 +21,77 @@ function loadData() {
     .catch((error) => alert(error));
 }
 function showCartData() {
-  //console.log(cartInfo.length)
   document.getElementById("menu-item").style.display = "none";
-  document.getElementById("cart-item").style.display = "flex";
-  let list = document.getElementById("cart-item");
-  list.innerHTML = "";
-  let total = 0;
+  document.getElementById("cart").style.display = "block";
+  let cart = document.getElementById("cart");
+  cart.innerHTML = "";
+
+  if (cartInfo.length === 0) {
+    cart.innerHTML = `
+        <p class="text-3xl text-center">Cart is empty. You need to add items!</p>
+    `;
+    return;
+  }
+
+  let itemsList = document.createElement("div");
+  itemsList.innerHTML = "";
+  let cartTotal = 0;
 
   cartInfo.forEach((item, i) => {
-    total = total + item.qty * item.price;
+    let itemTotal = 0;
+    itemTotal = itemTotal + item.qty * item.price;
+    cartTotal += itemTotal;
 
-    list.innerHTML += `
-            <div class="">
-                <span>${item.name}</span>
-                <div class="">
-                    <button onclick="updateQty(${i},-1)">-</button>
-            ${item.qty}
-                    <button onclick="updateQty(${i},1)">+</button>
-                    <span>${total}</span>
+    itemsList.innerHTML += `
+            <div class="flex justify-between bg-white mb-6">
+                <div class="flex items-center">
+                    <div class="w-30 h-30 mr-2">
+                        <img class="w-30 mr-4" src="${item.image}">
+                    </div>
+                    <div>
+                        <div>${item.name}</div>
+                        <div class="italic text-blue-300">${item.cuisine}</div>
+                        <div>Rating: ${item.rating}</div>
+                    </div>
+                </div>
+                <div class="p-2 flex flex-col justify-center">
+                    <div class="text-end">${itemTotal}$</div>
+                    <div class="flex flex-nowrap items-center">
+                        <button class="text-xl rounded-full px-2 mr-2 bg-gray-100 inline-block" onclick="updateQty(${i},-1)">-</button>
+                        <span>${item.qty}</span>
+                        <button class="text-xl rounded-full px-2 ml-2 bg-gray-100 inline-block" onclick="updateQty(${i},1)">+</button>
+                    </div>
                 </div>
             </div>
         `;
   });
+
+  let cartFooter = document.createElement("div");
+  cartFooter.className = "flex justify-between items-center";
+
+  let cartTotalElement = document.createElement("span");
+  cartTotalElement.className = "text-3xl font-bold";
+  cartTotalElement.innerText = "Total: " + cartTotal + "$";
+
+  let cartPayButton = document.createElement("button");
+  cartPayButton.className =
+    "bg-blue-600 text-white py-2 px-10 rounded-lg hover:bg-blue-700 transition";
+  cartPayButton.innerText = "Pay";
+
+  cartPayButton.addEventListener("click", () => {
+    alert("Order successfully created!");
+  });
+  cartFooter.appendChild(cartTotalElement);
+  cartFooter.appendChild(cartPayButton);
+
+  cart.appendChild(itemsList);
+  cart.appendChild(cartFooter);
 }
 
 function updateQty(index, change) {
-  console.log("event called.");
+  if (change === -1 && cartInfo[index].qty === 0) {
+    return;
+  }
   cartInfo[index].qty += change;
   console.log(cartInfo);
   showCartData();
@@ -60,7 +105,7 @@ function renderItems(items) {
     let div = document.createElement("div");
 
     div.className =
-      "flex flex-col p-2 m-4 shadow-xl rounded-lg w-full sm:w-1/3 md:w-1/4";
+      "flex flex-col p-2 m-4 shadow-xl rounded-lg w-full sm:w-1/3 md:w-1/4 relative";
 
     let img = document.createElement("img");
     img.src = menu.image;
@@ -72,7 +117,22 @@ function renderItems(items) {
 
     let price = document.createElement("p");
     price.className = "text-center mb-2";
-    price.innerText = "Price: " + PRICE;
+    price.innerText = "Price: " + PRICE + "$";
+
+    let cuisine = document.createElement("p");
+    cuisine.className = "italic text-center text-blue-300";
+    cuisine.innerText = menu.cuisine;
+
+    let rating = document.createElement("div");
+    rating.className =
+      "absolute top-10 left-10 rounded-lg bg-black text-white px-2 flex items-center";
+    let ratingImage = document.createElement("img");
+    ratingImage.src = "assets/star.png";
+    ratingImage.className = "w-7";
+    let ratingText = document.createElement("span");
+    ratingText.innerText = menu.rating;
+    rating.appendChild(ratingImage);
+    rating.appendChild(ratingText);
 
     let addToCart = document.createElement("button");
     addToCart.className =
@@ -81,13 +141,22 @@ function renderItems(items) {
 
     div.appendChild(img);
     div.appendChild(p);
+    div.appendChild(cuisine);
     div.appendChild(price);
     div.appendChild(addToCart);
+    div.appendChild(rating);
 
     addToCart.addEventListener("click", () => {
       let result = cartInfo.find((cartItem) => cartItem.name == menu.name);
       if (result == undefined) {
-        cartInfo.push({ name: menu.name, qty: 1, price: PRICE });
+        cartInfo.push({
+          name: menu.name,
+          qty: 1,
+          price: PRICE,
+          image: menu.image,
+          rating: menu.rating,
+          cuisine: menu.cuisine,
+        });
         let cartBadge = document.getElementById("cartBadge");
         cartBadge.innerHTML = cartInfo.length;
 
@@ -101,7 +170,7 @@ function renderItems(items) {
 }
 
 function hideCart() {
-  document.getElementById("cart-item").style.display = "none";
+  document.getElementById("cart").style.display = "none";
 }
 
 function logout() {
